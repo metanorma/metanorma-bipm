e9f320b35ff248a11148eef6de155fc217da2b4e
Nick Nicholas <opoudjis@gmail.com>
2020-10-21 21:48:07 +1100

    guides: https://github.com/metanorma/metanorma-bipm/issues/7

--- lib/isodoc/bipm/html_convert.rb
+++ lib/isodoc/bipm/html_convert.rb
@@ -7,6 +7,21 @@ module IsoDoc
   module BIPM
     class HtmlConvert < IsoDoc::Generic::HtmlConvert
 
+      def middle(isoxml, out)
+        super
+        doccontrol isoxml, out
+      end
+
+      def doccontrol(isoxml, out)
+        c = isoxml.at(ns("//doccontrol")) or return
+        out.div **attr_code(class: "doccontrol") do |div|
+          clause_parse_title(c, div, c.at(ns("./title")), out)
+          c.children.reject { |c1| c1.name == "title" }.each do |c1|
+            parse(c1, div)
+          end
+        end
+      end
+
       include BaseConvert
       include Init
     end
--- lib/isodoc/bipm/presentation_xml_convert.rb
+++ lib/isodoc/bipm/presentation_xml_convert.rb
@@ -27,8 +27,83 @@ module IsoDoc
         super
       end
 
+      def conversions(docxml)
+        super
+        doccontrol docxml
+      end
+
+      def doccontrol docxml
+        return unless docxml.at(ns("//bibdata/relation[@type = 'supersedes']"))
+        clause = <<~END
+        <doccontrol>
+        <title>Document Control</title>
+        <table unnumbered="true"><tbody>
+        <tr><td>Authors:</td><td/><td>#{list_authors(docxml)}</td></tr>
+        <tr>#{list_draft(docxml, 1).map { |x| "<td>#{x}</td>" }.join }
+        <td>#{list_cochairs(docxml)}</td></tr>
+        <tr>#{list_draft(docxml, 2).map { |x| "<td>#{x}</td>" }.join }
+        <td>#{list_chairs(docxml)}</td></tr>
+        #{list_drafts(docxml)}
+        </tbody></table></doccontrol>
+        END
+        docxml.root << clause
+      end
+
+      def list_drafts(xml)
+        ret = ""
+        i = 3
+        while a = list_draft(xml, i)
+          ret += "<tr>#{list_draft(xml, i).map { |x| "<td>#{x}</td>" }.join }"\
+            "<td/></tr>"
+          i += 1
+        end
+        ret
+      end
+
+      def list_draft(xml, i)
+        return unless d =
+          xml.at(ns("//bibdata/relation[@type = 'supersedes'][#{i}]/bibitem"))
+        date = d&.at(ns("./date"))&.text
+        draft = d&.at(ns("./version/draft"))&.text and
+          draft = "Draft #{draft}"
+        edn = d&.at(ns("./edition"))&.text and
+          edn = "Edition #{edn}"
+        [date, [draft, edn].join(" ")]
+      end
+
+      def list_authors(xml)
+        ret = list_people(xml, "//bibdata/contributor[xmlns:role/@type = 'author']/person")
+        @i18n.multiple_and(ret, @i18n.get["and"])
+      end
+
+      def list_cochairs(xml)
+        ret = list_people(xml, "//bibdata/contributor[xmlns:role[contains(text(),'co-chair')]]/person")
+        role = xml&.at(ns("//bibdata/contributor[xmlns:role[contains(text(),'co-chair')]]/role"))&.text
+        label = ret.size > 1 && role ? "#{role}s" : role
+        "#{label}: #{@i18n.multiple_and(ret, @i18n.get["and"])}"
+      end
+
+      def list_chairs(xml)
+        ret = list_people(xml, "//bibdata/contributor[xmlns:role[contains(text(),'chair')][not(contains(text(),'co-chair'))]]/person")
+        role = xml&.at(ns("//bibdata/contributor[xmlns:role[contains(text(),'chair')][not(contains(text(),'co-chair'))]]/role"))&.text
+        label = ret.size > 1 && role ? "#{role}s" : role
+        "#{label}: #{@i18n.multiple_and(ret, @i18n.get["and"])}"
+      end
+
+      def list_people(xml, xpath)
+        ret = []
+        xml.xpath(ns(xpath)).each do |p|
+          name = p&.at(ns("./name/completename"))&.text
+          aff = p&.at(ns("./affiliation/organization/abbreviation"))&.text ||
+            p&.at(ns("./affiliation/organization/name"))&.text
+          c = name || ""
+          aff and c += " (#{aff})"
+          ret << c
+        end
+        ret
+      end
+
       include Init
     end
   end
 end
-
