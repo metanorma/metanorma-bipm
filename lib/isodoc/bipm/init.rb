1a445e8de3b16e0b15446323f360535e51d9bd3b
Nick Nicholas <opoudjis@gmail.com>
2025-10-15 00:11:31 +1100

    merge user-specified i18n with flavour i18n: https://github.com/metanorma/metanorma-pdfa/issues/7

--- lib/isodoc/bipm/init.rb
+++ lib/isodoc/bipm/init.rb
@@ -17,7 +17,8 @@ module IsoDoc
 
       def i18n_init(lang, script, locale, i18nyaml = nil)
         @i18n = I18n.new(lang, script, locale: locale,
-                                       i18nyaml: i18nyaml || @i18nyaml)
+                                       i18nyaml: [i18nyaml,
+          Metanorma::Bipm.configuration.i18nyaml, @i18nyaml])
       end
 
       def bibrenderer(options = {})
