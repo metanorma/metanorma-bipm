631ac552d09b7a4bc377c62fc35514ca8599ca3b
Nick Nicholas <opoudjis@gmail.com>
2025-11-02 00:49:44 +1100

    update relaton-render: https://github.com/metanorma/metanorma-jis/issues/388

--- lib/relaton/render/citations.rb
+++ lib/relaton/render/citations.rb
@@ -4,7 +4,7 @@ module Relaton
       # TODO different behaviour for JCGM = ISO
       def use_terminator?(ref, final, cit)
         !ref || ref.empty? and return false
-        cit[:data_liquid][:home_standard] and return false
+        cit[:data][:home_standard] and return false
         !ref.end_with?(final)
       end
     end
